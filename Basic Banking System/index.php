2ab1ac8fb94b79c439f4a9aad95aa8e063acbe0a
laksh1256 <lkkhandelwal11nov.com>
2021-02-18 12:42:31 +0530

    basic_banking_system

---
+++ Basic Banking System/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="css/style.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body  style="background-size: 100% 100% ; "> 
+
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+      
+            <div class="row intro py-1" >
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3> Welcome to</h3>
+                  <h1>DIGIBANK</h1>
+    
+      
+
+      <!-- Activity section -->
+            <div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="img/download.png" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><button style="background-color : orange">Create a User</button></a>
+                    </div>
+                  <div class="col-md act">
+                    <img src="img/tr.jpg" style="max-width: 55%" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color : red;">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/trh.jpg" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color : #2785C4;">Transaction History</button></a>
+                  </div>
+            </div>
+      </div>
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
